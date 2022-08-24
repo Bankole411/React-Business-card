c8bc888ad36433440e84aa690323f4d3aaf05c18
Bankole Oluwatobiloba <66980300+Bankole411@users.noreply.github.com>
2022-08-25 00:18:05 +0100

    React - Training wheels
    My first react project after taking a react basics course

---
+++ components/Interest.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Interest(){
+    return(
+        <div className="interest-section">
+            <h2>Interests</h2>
+            <h5>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</h5>
+        </div>
+    )
+}
